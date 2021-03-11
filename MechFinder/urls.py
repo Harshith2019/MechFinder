9f22e09ea93e04e793a4041925a70a7315ea10f2
Sree Nithish B <sreenithishbofficial@gmail.com>
2021-03-11 18:30:05 +0530

    adding customers url

--- MechFinder/urls.py
+++ MechFinder/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mechanic/', include('mechanic.urls')),
+    path('customer/', include('customer.urls')),
 ]
