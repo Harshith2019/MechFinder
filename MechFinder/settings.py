a3422cf882d195a55b745fcad3c7e9225646953a
Sree Nithish B <sreenithishbofficial@gmail.com>
2021-03-11 18:20:38 +0530

    Initializing customer app

--- MechFinder/settings.py
+++ MechFinder/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'channels',
 
     'mechanic',
+    'customer',
 ]
 
 MIDDLEWARE = [
--- mechanic/routing.py
+++ mechanic/routing.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .consumers import WSConsumer
 
 ws_urlpatterns = [
-    path('ws/some_url/', WSConsumer.as_asgi())
+    path('ws/mechanic_dashboard_url/', WSConsumer.as_asgi())
 ]
