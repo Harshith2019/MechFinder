2399eff8c4ca522983899d21861c3b5a83835ff4
Sree Nithish B <sreenithishbofficial@gmail.com>
2021-04-09 15:55:02 +0530

    Connecting websockets for history helps

--- mechanic/models.py
+++ mechanic/models.py
@@ -38,3 +38,9 @@ class helps_finished(models.Model):
 
     def __str__(self):
         return str(self.mechanic_name) + ' | ' + str(self.customer_name)
+
+    def get_all_objects(self):
+        queryset = self._meta.model.objects.all().values('mechanic_name', 'customer_name', 'email', 'customer_email', 'contact_no', 'customer_contact_no', 'latitude', 'customer_latitude', 'longitude', 'customer_longitude')
+        # can use the below method also
+        # queryset = self.__class__.objects.all()
+        return list(queryset)
