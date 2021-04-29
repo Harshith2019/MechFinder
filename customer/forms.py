f6a8b2975caacb03534e05b464002dc4c0bf51a7
Harshith2019 <harshithjupuru@gmail.com>
2021-04-29 19:28:57 +0530

    deleting db

---
+++ customer/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from .models import Location
+
+class LocationModelForm(forms.ModelForm):
+    class Meta:
+        model = Location
+        fields = ('latitude', 'longitude')
+        widgets = {
+            'latitude': forms.TextInput(attrs={'id': 'latitude_id'}),
+            'longitude': forms.TextInput(attrs={'id': 'longitude_id'}),
+        }
+        labels = {
+            'latitude': "",
+            'longitude': "",
+        }
+
+class AskHelpForm(forms.Form):
+    name = forms.CharField(label='', max_length=100, widget= forms.TextInput(attrs={'id':'name_AskHelpForm'}))
+    user_name = forms.CharField(label='', max_length=100, widget= forms.TextInput(attrs={'id':'userName_AskHelpForm'}))
+    email = forms.CharField(label='', max_length=100, widget= forms.TextInput(attrs={'id':'email_AskHelpForm'}))
+    description = forms.CharField(label='', max_length=1000, widget= forms.Textarea(attrs={'id':'description_AskHelpForm'}))
+    contact_no = forms.CharField(label='', max_length=100, widget= forms.TextInput(attrs={'id':'contact_no_AskHelpForm'}))
---
+++ customer/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class Location(models.Model):
+    latitude = models.CharField(max_length=200)
+    longitude = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.latitude},{self.longitude}"
+
+class helps_received(models.Model):
+    customer_name = models.CharField(max_length=200)
+    mechanic_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    contact_no = models.CharField(max_length=50)
+    latitude = models.CharField(max_length=200)
+    longitude = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.customer_name)
+
+    def get_all_objects(self):
+        queryset = self._meta.model.objects.all().values('mechanic_name', 'customer_name', 'email', 'contact_no', 'latitude', 'longitude')
+        # can use the below method also
+        # queryset = self.__class__.objects.all()
+        return list(queryset)
