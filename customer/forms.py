9cfffa1d072fe3b2e41439d2c6a9bde817ea0bca
Sree Nithish B <sreenithishbofficial@gmail.com>
2021-05-05 21:13:19 +0530

    decorated customer front-end

--- customer/forms.py
+++ customer/forms.py
@@ -18,5 +18,5 @@ class AskHelpForm(forms.Form):
     name = forms.CharField(label='', max_length=100, widget= forms.TextInput(attrs={'id':'name_AskHelpForm'}))
     user_name = forms.CharField(label='', max_length=100, widget= forms.TextInput(attrs={'id':'userName_AskHelpForm'}))
     email = forms.CharField(label='', max_length=100, widget= forms.TextInput(attrs={'id':'email_AskHelpForm'}))
-    description = forms.CharField(label='', max_length=1000, widget= forms.Textarea(attrs={'id':'description_AskHelpForm'}))
+    description = forms.CharField(label='', max_length=1000, widget= forms.Textarea(attrs={'id':'description_AskHelpForm', 'style': 'height: 200px;width:500px'}))
     contact_no = forms.CharField(label='', max_length=100, widget= forms.TextInput(attrs={'id':'contact_no_AskHelpForm'}))
